5059b0ca7215e526c29a7fb7341b1cb16a9f6c25
Andrew Thomas <andrewmaltezthomas@hotmail.com>
2015-02-23 22:42:19 -0300

    Delete motif_search.py

--- motif_search.py
+++
@@ -1,8 +0,0 @@
-import re
-print
-sequence = raw_input("Enter sequence to search for motif: ")
-motif = raw_input("Enter motif to search for: ")
-motif_search = re.finditer('(?=%s)' % motif, sequence)
-print
-print "Found motif start in positions:"
-print str([item.start(0)+1 for item in motif_search])
