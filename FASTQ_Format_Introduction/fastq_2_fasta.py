c4b561cd0e3c2138f0956b75f957eb1ac0ef0ee7
andrew.thomas <andrewmaltezthomas@hotmail.com>
2015-03-05 21:46:30 -0300

    How to Handle Quality

---
+++ FASTQ_Format_Introduction/fastq_2_fasta.py
@@ -0,0 +1,7 @@
+from Bio import SeqIO
+sequence_file = raw_input("Enter sequence file name: ")
+out_handle = open('Sequence.fasta', 'w')
+SeqIO.convert(sequence_file, "fastq", out_handle, "fasta")
+out_handle.close()
+
+
