1be069d0a135879b3d7d1c9c0c593a5f88323eff
Andrew Thomas <andrewmaltezthomas@hotmail.com>
2015-03-05 21:51:09 -0300

    A Rapid Introduction to Molecular Biology

--- 1.Counting_DNA_Nucleotides/count_nucleotides.py
+++ 1.Counting_DNA_Nucleotides/count_nucleotides.py
@@ -1,3 +1,8 @@
+"""
+What this script does:
+Input: A DNA string s of length at most 1000 nt.
+Return: Four integers (separated by spaces) counting the respective number of times that the symbols 'A', 'C', 'G', and 'T' occur in s.
+"""
 #Open File
 sequence = open("sequence_file.txt", 'r')
 sequence_file = sequence.read()
